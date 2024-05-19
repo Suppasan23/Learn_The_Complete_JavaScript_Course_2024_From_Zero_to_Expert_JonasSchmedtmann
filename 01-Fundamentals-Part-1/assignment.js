a0515be5f3ccf4a4bac5fb94de8badc20e0cc041
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2024-05-19 17:29:12 +0700

    Finish12

---
+++ 01-Fundamentals-Part-1/assignment.js
@@ -0,0 +1,7 @@
+let country = "Thailand";
+let continent = "Asia";
+let population = 65000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,57 @@
+/* let js = "amazing";
+
+console.log(40 + 8 + 23 - 10);
+
+console.log("Suppasan");
+
+console.log(23);
+
+let firstName = "Matilda";
+let first = "Suppasan";
+let firstNamePerson;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let dante_beatrice = "DB";
+
+let $function = 97;
+
+let Person = "jonas";
+
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+
+let age = 23; //Number
+
+let customerName = "billick"; //String
+
+let children; //Undefine, Null
+ */
+
+let javascriptIsFun = true;
+let myAge = 23;
+let myPet = "cat";
+
+/* console.log(typeof true); */
+console.log(typeof javascriptIsFun);
+/* console.log(typeof myAge);
+console.log(typeof myPet); */
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
