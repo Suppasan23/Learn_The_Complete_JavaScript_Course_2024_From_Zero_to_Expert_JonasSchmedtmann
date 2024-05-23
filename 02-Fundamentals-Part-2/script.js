1d1aa839ba76b71c000a632966f02a902118861e
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2024-05-23 22:16:45 +0700

    finish 41

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -60,7 +60,7 @@ function cutFruitPieces(fruit) {
 console.log(fruitProcessor("apple", "orange"));
 console.log(fruitProcessor("grape", "melon"));
  */
-
+/* 
 const yearsUntilRetirement = function (birthYear, firstName) {
   const age = calcAge(birthYear);
   const retirement = 60 - age;
@@ -88,3 +88,90 @@ function checkWinner(avgDolphins, avgKoalas) {
     return `Koalas win (${avgDolphins} vs. ${avgKoalas})`;
   }
 }
+ */
+
+/* const friends = ["Mikey", "Steve", "Harry"];
+console.log(friends);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jenna";
+console.log(friends);
+
+const job = "Gamer";
+const suppasan = ["Suppasan", "Chaianantakul", 2024 - 1993, job, friends];
+
+console.log(suppasan);
+
+// Exercise
+const calcAge = function (birthYear) {
+  return 2024 - birthYear;
+};
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[0]),
+  calcAge(years[years.length - 1]),
+]; */
+
+const friends = ["Mikey", "Steve", "Harry"];
+
+// Add element
+const newLenght = friends.push("Paul");
+console.log(friends);
+console.log(newLenght);
+
+friends.unshift("John");
+console.log(friends);
+
+// Remove element
+const detachElement = friends.pop(); // Detach last element
+console.log(friends);
+console.log(detachElement);
+
+friends.shift();
+console.log(friends);
+
+// Check element
+console.log(friends.indexOf("Steve"));
+console.log(friends.indexOf("bob"));
+
+friends.push(23);
+
+console.log(friends.includes("Steve"));
+console.log(friends.includes("bob"));
+
+if (friends.includes(friends[2])) {
+  console.log(`You have a friend called ${friends[2]}`);
+} else {
+  console.log(`You have not a friend called ${friends[2]}`);
+}
+
+function calcTip(bill) {
+  if (50 <= bill && bill <= 300) {
+    return bill * (15 / 100);
+  } else {
+    return bill * (20 / 100);
+  }
+}
+
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+console.log(tips);
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
