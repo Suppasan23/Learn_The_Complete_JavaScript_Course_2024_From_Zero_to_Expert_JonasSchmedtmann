1fa8f9328e3ef3287d501438b05d09168be03836
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2024-05-23 16:22:55 +0700

    finish 38

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -10,7 +10,7 @@ function fruitProcessor(fruit1, fruit2) {
 console.log(fruitProcessor("apple", "orange"));
 console.log(fruitProcessor("grape", "melon")); */
 
-// Function declaration
+/* // Function declaration
 function calcAge1(birthYear) {
   return 2037 - birthYear;
 }
@@ -40,4 +40,51 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 };
 
 console.log(yearsUntilRetirement(1993, "Benz"));
-console.log(yearsUntilRetirement(1980, "Ben"));
+console.log(yearsUntilRetirement(1980, "Ben")); */
+
+/* // Declare / Create Function
+function fruitProcessor(fruit1, fruit2) {
+  const fruit1Pieces = cutFruitPieces(fruit1);
+  const fruit2Pieces = cutFruitPieces(fruit2);
+
+  const juices1 = `This is a juice of ${fruit1Pieces}`;
+  const juices2 = `This is a juice of ${fruit2Pieces}`;
+  return juices1, juices2;
+}
+
+function cutFruitPieces(fruit) {
+  return `4 pieces ${fruit} `;
+}
+
+// Calling / Running / Invoking Function
+console.log(fruitProcessor("apple", "orange"));
+console.log(fruitProcessor("grape", "melon"));
+ */
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 60 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired 🎃`);
+    return -1;
+  }
+};
+
+const calcAge = function (birthYear) {
+  return 2024 - birthYear;
+};
+
+console.log(yearsUntilRetirement(1993, "Benz"));
+console.log(yearsUntilRetirement(1980, "Benjamin"));
+
+function checkWinner(avgDolphins, avgKoalas) {
+  if (avgDolphins > avgKoalas) {
+    return `Dolphins win (${avgDolphins} vs. ${avgKoalas})`;
+  } else if (avgDolphins > avgKoalas) {
+    return `Koalas win (${avgDolphins} vs. ${avgKoalas})`;
+  }
+}
