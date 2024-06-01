771c831cbf2c3747aec343b1ad9f68ef54353ea1
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2024-06-01 21:17:40 +0700

    finish80

---
+++ 06-Modal/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const btnOpenModal = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
+const btnCloseModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener("click", () => openModal());
+}
+
+btnCloseModal.addEventListener("click", () => closeModal());
+overlay.addEventListener("click", () => closeModal());
