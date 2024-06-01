8bcb5f45d3472be568517189988288c3c06cad88
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2024-06-01 18:21:43 +0700

    finish 79

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,12 +1,96 @@
 "use strict";
 
-console.log(document.querySelector(".message").textContent);
+let secretNumber;
+let score;
+let highScore;
 
-document.querySelector(".message").textContent = "Correct Number!🎊🎉";
+prepareState(0);
+prepareHiScore(0);
 
-console.log(document.querySelector(".message").textContent);
+function setMessage(setMessage) {
+  document.querySelector(".message").textContent = setMessage;
+}
 
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 13;
+function setHighScore(setHighscore) {
+  document.querySelector(".highscore").textContent = setHighscore;
+}
 
-document.querySelector(".guess").value = 50;
+function setScore(setScore) {
+  document.querySelector(".score").textContent = setScore;
+}
+
+function setNumber(setNumber) {
+  document.querySelector(".number").textContent = setNumber;
+}
+
+function setGuessValue(setGuess) {
+  document.querySelector(".guess").value = setGuess;
+}
+
+function getGuessValue() {
+  return document.querySelector(".guess").value;
+}
+
+function prepareState(state) {
+  if (state === 0) {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    score = 20;
+    setScore(score);
+    setGuessValue("");
+  }
+  setNumber(state === 0 ? "?" : secretNumber);
+  setMessage(state === 0 ? "Start guessing..." : "🎉 Correct Number!");
+  document.querySelector("body").style.backgroundColor =
+    state === 0 ? "#222333" : "#60b347";
+  document.querySelector(".number").style.width =
+    state === 0 ? "15rem" : "30rem";
+  document.querySelector(".number").style.height =
+    state === 0 ? "10rem" : "15rem";
+}
+
+function prepareHiScore(state) {
+  if (state === 0) {
+    highScore = 0;
+    setHighScore(highScore);
+  } else if (state === 1) {
+    if (score > highScore) {
+      highScore = score;
+      setHighScore(highScore);
+    }
+  }
+}
+
+function checkHighLow(state) {
+  if (score > 1) {
+    score--;
+    setMessage(state ? "📈Too High!" : "📉Too Low!");
+    setScore(score);
+  } else {
+    setMessage("😭You lose the game");
+    setScore(0);
+  }
+}
+
+document.querySelector(".again").addEventListener("click", () => {
+  prepareState(0);
+});
+
+document.querySelector(".guess").addEventListener("click", () => {
+  setGuessValue("");
+});
+
+document.querySelector(".check").addEventListener("click", () => {
+  const guess = Number(getGuessValue());
+
+  if (!guess) {
+    setMessage("⛔ No number!");
+    return;
+  }
+
+  if (guess === secretNumber) {
+    prepareState(1);
+    prepareHiScore(1);
+  } else {
+    checkHighLow(guess > secretNumber);
+  }
+});
